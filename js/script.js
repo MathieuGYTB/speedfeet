2e1bb64750afc6d513eb92638ebd52c1338873f7
MathieuGYTB <mat.68@orange.fr>
2022-05-23 08:07:29 +0200

    add an advert alert

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+  alert(`ATTENTION ! Ce site est fictif ! Rien n'est vrai : ni l'association ni les évènements. Il a été crée dans le cadre d'une évaluation pendant une formation de développeur web`)
+})
+let myModal = document.getElementById('advertissement');
+myModal.onload((ev) => {
+
+})
